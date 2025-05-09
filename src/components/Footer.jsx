2cb2594a5fab52203dea48fdad3f32403fc55e99
Sanjeev <sanjeevaler57@gmail.com>
2025-05-09 08:43:08 +0545

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,30 +2,30 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center items-center flex justify-between'>
+    <div className='text-center items-center flex justify-between bg-[#0A1E35]'>
       
      
      <div className='pl-6'> 
-        <p>Homes.com</p>
-        <p>Banepa,Kavre 449900</p>
-        <p>+9779860437219</p>
+        <p className='text-gray-300'>Homes.com</p>
+        <p className='text-gray-300'>Banepa,Kavre 449900</p>
+        <p className='text-gray-300'>+9779860437219</p>
       </div>
   
 
       <div className='items-center'>
-          <p>Apartments</p> 
-          <p>House</p>
-          <p>Shop Spaces</p> 
-          <p>Land</p>
-          <p>Office Spaces</p> 
-          <p>Flats</p> 
+          <p className='text-gray-300'>Apartments</p> 
+          <p className='text-gray-300'>House</p>
+          <p className='text-gray-300'>Shop Spaces</p> 
+          <p className='text-gray-300'>Land</p>
+          <p className='text-gray-300'>Office Spaces</p> 
+          <p className='text-gray-300'>Flats</p> 
       </div>
 
       <div className='pr-6 items-center'>
-        <p>Follow Us</p>
-        <p>Facebook</p>
-        <p>Instagram</p>
-        <p>Twitter</p>
+        <p className='text-gray-300'>Follow Us</p>
+        <p className='text-gray-300'>Facebook</p>
+        <p className='text-gray-300'>Instagram</p>
+        <p className='text-gray-300'>Twitter</p>
       </div>
     </div>
   )
--- src/components/pages/Property/Property.jsx
+++ src/components/pages/Property/Property.jsx
@@ -3,11 +3,53 @@ import logo from '../../../assets/images/hero1.jpg'
 
 export const Property = () => {
   return (
-    <div>
-        <div className=''>
-<h1>Property</h1>
-<h1>Test</h1>
+    <div className='flex justify-between' style={{backgroundImage: "url(../../../assets/images/img3.png)"}}>
+
+<section class="bg-[#0A2342] text-white py-20" style={{backgroundImage: "url(../../../assets/images/img3.png)"}}>
+  <div className='mx-auto px-6 md:px-12 items-center'>
+      <div className='w-full'>
+        <h1 className='text-4xl md:text-5xl font-bold leading-tight mb-6'>
+          Find Your Dream Home
+        </h1>
+        <p>
+          Explore Premium properties, affordable home in your area.
+        </p>
+        <a>
+          Browse Listings
+        </a>
+      </div>  
+  </div>
+  {/* <div class="container mx-auto px-6 md:px-12 flex flex-col md:flex-row items-center">
+   
+    <div class="w-full md:w-1/2 mb-10 md:mb-0">
+      <h1 class="text-4xl md:text-5xl font-bold leading-tight mb-6">
+        Find Your Dream Home
+      </h1>
+      <p class="text-lg mb-8 text-gray-300">
+        Explore premium listings, luxury properties, and affordable homes in your area.
+      </p>
+      <a href="#listings" class="bg-white text-[#0A2342] font-semibold py-3 px-6 rounded-xl shadow hover:bg-gray-100 transition">
+        Browse Listings
+      </a>
+    </div>
+
+    <div class="w-full md:w-1/2">
+      <img src="" alt="Beautiful Property" class="rounded-2xl shadow-lg" />
+    </div>
+  </div> */}
+</section>
+
+        {/* <div className=''>
+            <p>Property</p>
         </div>
+
+        <div className=''>
+            <p>property</p>
+        </div>
+
+        <div className=''>
+            Property3
+        </div> */}
   </div>
 
   )
