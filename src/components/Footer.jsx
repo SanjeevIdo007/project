634005ed99a33d1461f15e183561c41a15106f69
Sanjeev <sanjeevaler57@gmail.com>
2025-05-18 17:54:57 +0545

    prac

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='text-center items-center flex justify-between'>
       
-     
+     {/* jksjsd */}
      <div className='pl-6 '> 
         <p className='text-gray-300'>Homes.com</p>
         <p className='text-gray-300'>Banepa,Kavre 449900</p>
